5fc1fbf9c4cc63366eebd9c0004a3987d74ea4c2
JoshXie <61863381+JoshXie0809@users.noreply.github.com>
2022-05-03 14:27:43 +0800

    with 2^n, I think can compute faster
    2^n

--- note/note_001_matrixPower.R
+++ note/note_001_matrixPower.R
@@ -1,1 +1,22 @@
+`%^%` <- function(K, n) {
+  I <- diag(ncol(K))
+  power <- function(A, n) {
+    if( n %/% 2 >= 1 ) {
+      B <- ifelse( A %% 2 == 1, A, I)
+      A <- A %*% A
+      return(B %*% power(A, n %/% 2))
+    }else{
+      return(A)
+    }
+  }
+  return(power(K, n))
+}
+
+K <- matrix(
+  c(1/5, 2/5, 2/5,
+    0, 0, 1,
+    1/2, 0, 1/2), ncol = 3, byrow = T
+)
+
+K %^% 1000
 
