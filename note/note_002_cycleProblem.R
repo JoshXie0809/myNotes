729b6c770c21ba95eb7a8f04b73a73a8d144ccee
Josh <61863381+JoshXie0809@users.noreply.github.com>
2022-05-04 23:45:19 +0800

    Update note_002_cycleProblem.R
    add check method

--- note/note_002_cycleProblem.R
+++ note/note_002_cycleProblem.R
@@ -3,7 +3,9 @@
 
 Container <- setRefClass(
     "Container", fields = list(cycle = "numeric"),
-    methods = list( add = function(newItem) { cycle <<- c(cycle, newItem) } )
+    methods = list( 
+        add = function(newItem) { cycle <<- c(cycle, newItem) },
+        check = function(Item) { Item %in% cycle } )
 )
 
 # create a cycle with initial value
@@ -13,3 +15,15 @@ Cycle1
 # add next
 Cycle1$add(23)
 Cycle1
+
+# check 3 in this cycle?
+Cycle1$check(3)
+# > FALSE
+
+# check 2 in this cycle?
+Cycle1$check(2)
+# > TRUE
+
+
+
+
