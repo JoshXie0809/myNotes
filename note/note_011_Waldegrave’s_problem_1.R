c1f125e3d9fffd9f1292646bf1b308b4b2f39745
Josh <61863381+JoshXie0809@users.noreply.github.com>
2022-05-20 06:42:05 +0800

    Update note_011_Waldegrave’s_problem_1.R
    finished

--- note/note_011_Waldegrave’s_problem_1.R
+++ note/note_011_Waldegrave’s_problem_1.R
@@ -1,1 +1,36 @@
+# flip a coin with probability p to have a head
+# write head = 1, number = 0
+# what is the expected value
+# when head appears countinuouslly n times
+# for n = 3,
+# 111, 3 times to have "111"
+# 0111, 4 times to have "111"
+# 100111, 6 times to have "111"
+
+library(Rcpp)
+
+cpptxt <- '
+    int allOne( int oneNum, double prob ) {
+        int count = 0, trial = 0;
+        while( count < oneNum ) {
+           if(runif(1)[0] <= prob ) {
+                count++;
+           } else {
+                count = 0;
+           }
+           trial++;
+        }
+        return trial;
+    }
+'
+cppFunction(cpptxt)
+
+simu <- function(oneNum, prob, B = 1e4) 
+    replicate(B, allOne(oneNum, prob))
+
+
+# to have "11", p = 0.5
+# mean(simu(2, .5))
+
+
 
