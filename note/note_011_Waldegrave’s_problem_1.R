31a54d12ca63b4ea408c6c881fb9c4393e75ba06
Josh <61863381+JoshXie0809@users.noreply.github.com>
2022-05-20 06:51:18 +0800

    Update note_011_Waldegrave’s_problem_1.R
    no replicate

--- note/note_011_Waldegrave’s_problem_1.R
+++ note/note_011_Waldegrave’s_problem_1.R
@@ -10,23 +10,28 @@
 library(Rcpp)
 
 cpptxt <- '
-    int allOne( int oneNum, double prob ) {
-        int count = 0, trial = 0;
-        while( count < oneNum ) {
-           if(runif(1)[0] <= prob ) {
-                count++;
-           } else {
-                count = 0;
-           }
-           trial++;
+    NumericVector allOne( int oneNum, double prob, int B ) {
+        NumericVector res(B);
+        int count, trial;
+
+        for(int i = 0; i < B; i++) {
+            count = 0; trial = 0;
+            while( count < oneNum ) {
+                if(runif(1)[0] <= prob ) {
+                        count++;
+                } else {
+                        count = 0;
+                }
+                trial++;
+            }
+            res[i] = trial;
         }
-        return trial;
+        return res;
     }
 '
 cppFunction(cpptxt)
 
-simu <- function(oneNum, prob, B = 1e4) 
-    replicate(B, allOne(oneNum, prob))
+simu <- function(oneNum, prob, B = 1e4)  allOne(oneNum, prob, B)
 
 
 # to have "11", p = 0.5
